f74935894e981b6825b364c31e35614a3774b512
Linus Johansson <golfarenlj@gmail.com>
2024-11-01 22:21:54 +0000

    Initial Django structure

---
+++ project_root/urls.py
@@ -0,0 +1,9 @@
+"""URL configuration."""
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('accounts/', include('allauth.urls')),
+    path('admin/', admin.site.urls),
+]
