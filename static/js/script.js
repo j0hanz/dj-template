78a597b50d80de0b6971081372bf536b486356b9
j0hanz <golfarenlj@gmail.com>
2025-03-25 23:16:25 +0100

    Add click event to count button and display count

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Get the button and span elements
   const countButton = document.getElementById("countButton");
+  // Get the span element
   const countSpan = document.getElementById("count");
+  // Initialize count
   let count = 0;
 
+  // Add event listener to button
   countButton.addEventListener("click", function () {
     count++;
     countSpan.textContent = count;
