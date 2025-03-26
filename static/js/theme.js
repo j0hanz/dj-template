760333cc3c59fbaffff3e98d449de0842b6bebea
j0hanz <golfarenlj@gmail.com>
2025-03-26 13:40:57 +0100

    Refactor theme management in JavaScript to improve clarity and functionality

--- static/js/theme.js
+++ static/js/theme.js
@@ -3,6 +3,7 @@
   const storedTheme = localStorage.getItem("theme");
   const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
+  // Set the theme based on stored preference or system preference
   if (storedTheme) {
     document.documentElement.setAttribute("data-bs-theme", storedTheme);
   } else if (prefersDark) {
@@ -15,27 +16,27 @@ document.addEventListener("DOMContentLoaded", function () {
   const darkModeIcon = document.getElementById("darkModeIcon");
   const lightModeIcon = document.getElementById("lightModeIcon");
 
+  // Check if the toggle button exists
   if (darkModeToggle) {
-    // Get current theme
-    const currentTheme = document.documentElement.getAttribute("data-bs-theme");
-
-    // Initialize icons based on current theme
-    updateIcons(currentTheme);
-
+    updateIcons(document.documentElement.getAttribute("data-bs-theme"));
     // Toggle theme when button is clicked
     darkModeToggle.addEventListener("click", function () {
       const currentTheme =
         document.documentElement.getAttribute("data-bs-theme");
       const newTheme = currentTheme === "dark" ? "light" : "dark";
-
-      document.documentElement.setAttribute("data-bs-theme", newTheme);
-      localStorage.setItem("theme", newTheme);
-      updateIcons(newTheme);
+      setTheme(newTheme);
     });
   }
 
+  // Function to set the theme and update local storage
+  function setTheme(theme) {
+    document.documentElement.setAttribute("data-bs-theme", theme);
+    localStorage.setItem("theme", theme);
+    updateIcons(theme);
+  }
+
+  // Function to update icons based on the current theme
   function updateIcons(theme) {
-    // Update icons based on theme
     if (theme === "dark") {
       darkModeIcon.classList.add("d-none");
       lightModeIcon.classList.remove("d-none");
